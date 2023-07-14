08653f8c9d9234ec60f4f491fd79d724942bd1b8
GGabriel199 <gabrielflorencio@outlook.com>
2023-07-13 22:38:53 -0300

    Android things.

---
+++ Assets/Objects/Scripts/Button/OpenGGamingStore.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGGamingStore : MonoBehaviour
+{
+    public void Url(){
+        Application.OpenURL("https://play.google.com/store/apps/dev?id=9098832117526373788&hl");
+    }
+}
